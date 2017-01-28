573a3c8d213986e476f3550bed4829e1c18fdb7a
jaredbebb <j.bebb@live.com>
2017-01-28 17:57:42 -0600

    3rd commit

---
+++ src/face/Buttons.java
@@ -0,0 +1,28 @@
+package face;
+
+public class Buttons {
+	
+	public int pressAdd(int num1, int num2){
+		int equals;
+		equals = num1+num2;
+		
+		return equals;
+	}
+	public int pressSubtract(int num1, int num2){
+		int equals;
+		equals = num1-num2;
+		
+		return equals;
+	}
+	public int pressMultiply(int num1, int num2){
+		int equals;
+		equals = num1*num2;
+		return equals;
+	}
+	public int pressDivide(int num1, int num2){
+		int equals;
+		equals = num1/num2;
+		return equals; 
+	}
+
+}
